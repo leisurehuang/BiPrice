23b74e4649b53ed773c0adf8cf9a1dcd783168a3
Lei Huang <leihuang@thoughtworks.com>
2025-01-01 11:09:25 +0800

    Initial Commit

---
+++ BiPrice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BiPrice
+//
+//  Created by lei huang on 2025/1/1.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
