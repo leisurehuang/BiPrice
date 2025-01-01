42ca178bff4202e9b00e1bb0ea561d65e48829c6
Lei Huang <leisure.huang34@gmail.com>
2025-01-01 15:21:46 +0800

    code fix after formart

--- BiPrice/AppDelegate.swift
+++ BiPrice/AppDelegate.swift
@@ -9,63 +9,55 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var window: NSWindow?
-
     var statusItem: NSStatusItem?
-    
-    let allCurrencies = ["bitcoin", "ethereum", "dogecoin", "litecoin"]  // 可选的币种列表
-    var currentCurrencyIndex = 0  // 当前请求的币种索引
-    
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    var currentCurrencyIndex = 0 // 当前请求的币种索引
+    let allCurrencies = ["bitcoin", "mask","ethereum", "dogecoin", "litecoin"] // 可选的币种列表
+
+    func applicationDidFinishLaunching(_: Notification) {
         // Insert code here to initialize your application
         // 创建状态栏项
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        
+
         // 设置状态栏图标
         if let button = statusItem?.button {
             button.title = "Loading..." // 默认显示文本
         }
-        window = nil
-
         // 更新比特币价格
         fetchBitcoinPrice()
-        
+
         // 设置刷新间隔，每隔60秒刷新一次
         Timer.scheduledTimer(timeInterval: 15, target: self, selector: #selector(fetchBitcoinPrice), userInfo: nil, repeats: true)
-        
     }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
+
+    func applicationWillTerminate(_: Notification) {
         // Insert code here to tear down your application
     }
-    
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+
+    func applicationSupportsSecureRestorableState(_: NSApplication) -> Bool {
         return true
     }
-    
+
     @objc func fetchBitcoinPrice() {
         let currency = allCurrencies[currentCurrencyIndex]
         guard let url = URL(string: "https://api.coincap.io/v2/rates/\(currency)") else { return }
-        
+
         // 创建 URLSession 来获取比特币价格
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
             if let data = data, error == nil {
                 do {
                     // 解析 JSON 数据
                     if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                         print("json:\(json)")
-                        
-                        if  let data = json["data"] as? [String: Any],
-                            let symbol = data["symbol"] as? String,
-                            let rateUsd = data["rateUsd"] as? String,
-                            let rate = Double(rateUsd) {
-                            
+
+                        if let data = json["data"] as? [String: Any],
+                           let symbol = data["symbol"] as? String,
+                           let rateUsd = data["rateUsd"] as? String,
+                           let rate = Double(rateUsd)
+                        {
                             // 在主线程更新 UI
                             DispatchQueue.main.async { [weak self] in
                                 guard let self else { return }
                                 self.statusItem?.button?.title = "\(symbol): $\(String(format: "%.4f", rate))"
-                                currentCurrencyIndex = (currentCurrencyIndex + 1) % allCurrencies.count
                             }
                         }
                     }
@@ -74,8 +66,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 }
             }
         }
-        
+
         task.resume()
+        currentCurrencyIndex = (currentCurrencyIndex + 1) % allCurrencies.count
     }
 }
-
